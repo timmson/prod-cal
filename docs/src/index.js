04393d5af7c66efceffbb609271e1fd5402586d5
timmson <timmson666@mail.ru>
2019-08-19 22:43:48 +0300

    #3

--- docs/src/index.js
+++ docs/src/index.js
@@ -4,19 +4,60 @@ import "./index.scss";
 import Calendar from "../../index.js";
 
 import Vue from "vue"
+import Moment from "moment";
 
 let app = new Vue({
     el: '#app',
     data: {
-        currentYear: parseInt(new Date().getFullYear().toString()),
+        currentYear: parseInt(Moment().format("YYYY")),
         calendar: new Calendar("ru"),
-        years: {}
+        selectedYear: {},
+        year: {}
     },
-    methods: {},
+    methods: {
+        buildCalendar: function () {
+            this.year = this.calendar.getYear(this.selectedYear).map((m, index) => {
+                    let momentMonth = Moment([this.selectedYear, index]);
+                    let month = {
+                        name: momentMonth.format("MMMM"),
+                        weeks: [],
+                    };
+                    let monthStartsWith = momentMonth.clone().startOf("month").format("d") - 1;
+                    for (let i = 0; i < 6; i++) {
+                        let week = [];
+                        for (let j = 0; j < 7; j++) {
+                            let cDay = i * 7 + j - monthStartsWith;
+                            if (cDay >= 0 && cDay < m.length) {
+                                week[j] = {
+                                    date: cDay + 1,
+                                    type: m[cDay]
+                                }
+                            } else {
+                                if (cDay < 0) {
+                                    week[j] = {
+                                        date: momentMonth.clone().startOf("month").add(cDay, "d").format("D"),
+                                        type: "inactive"
+                                    }
+                                } else {
+                                    week[j] = {
+                                        date: momentMonth.clone().endOf("month").add(cDay - m.length + 1, "d").format("D"),
+                                        type: "inactive"
+                                    }
+                                }
+                            }
+                        }
+                        month.weeks[i] = week;
+                    }
+                    return month;
+                }
+            );
+            console.log(this.year);
+        }
+    },
+    created() {
+        this.selectedYear = this.currentYear;
+    },
     mounted() {
-        [this.currentYear, this.currentYear + 1].forEach(y => this.$set(this.years, y, this.calendar.getYear(y)));
-    },
-    beforeUpdate() {
-
+        this.buildCalendar();
     }
 });
