1effab011527fb09fef4273d0464f07a791386f2
timmson <timmson666@mail.ru>
2019-08-19 16:20:26 +0300

    #3

---
+++ docs/src/index.js
@@ -0,0 +1,23 @@
+import "bootstrap";
+import "./index.scss";
+
+import Calendar from "../../index.js";
+
+import Vue from "vue"
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        currentYear: parseInt(new Date().getFullYear().toString()),
+        calendar: new Calendar("ru"),
+        years: {}
+    },
+    methods: {},
+    mounted() {
+        [this.currentYear, this.currentYear + 1].forEach(y => this.$set(this.years, y, this.calendar.getYear(y)));
+        //this.$forceUpdate();
+    },
+    beforeUpdate() {
+
+    }
+});
